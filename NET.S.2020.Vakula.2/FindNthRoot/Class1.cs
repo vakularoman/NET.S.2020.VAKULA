852e5cdf9eb4cd86c78dbc24597de5d003ea3e8d
vakularoman <61694662+vakularoman@users.noreply.github.com>
2020-03-17 18:21:16 +0300

    Add files via upload

---
+++ NET.S.2020.Vakula.2/FindNthRoot/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FindNthRoot
+{
+    public static class NewtonRoot
+    {
+        /// <summary>
+        /// find number degree root with
+        /// given accuracy
+        /// </summary>
+        /// <param name="value">initial number</param>
+        /// <param name="degree">degree of the root</param>
+        /// <param name="accuracy">accuracy of expected number</param>
+        /// <returns></returns>
+        public static double FindNthRoot(double value,double degree,double accuracy)
+        {
+            if (degree < 0 || accuracy < 0) throw new ArgumentException();
+
+            double next_val = value / degree;
+            double prev_val;
+            //xn+1 = xn - f(x)/f'(x)
+            do
+            {
+                prev_val = next_val;
+                next_val = prev_val - (Math.Pow(prev_val, degree) - value) /
+                    (degree * Math.Pow(prev_val, degree - 1));
+
+            } while (Math.Abs(prev_val - next_val) > accuracy);
+            //fractional part length = accuracy
+            return Math.Round(next_val - next_val% accuracy, accuracy.ToString().Length - 2);
+            
+        }
+    }
+}
---
+++ NET.S.2020.Vakula.2/InsertNumber/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InsertNumber
+{
+    public class BitInsert
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numberSource">number into which numbers are inserted</param>
+        /// <param name="numberChange">the number from which the data is taken</param>
+        /// <param name="i">right border of the insert</param>
+        /// <param name="j">left border of the insert</param>
+        /// <returns></returns>
+        public static int InsertNumber(int numberSource, int numberChange,int i ,int j)
+        {
+
+            if (i > j || i > 32 || i < 0 || j > 31 || j < 0) throw new ArgumentException();
+            // j - i + 1 ones
+            int segment = (2 << (j - i)) - 1;
+            //insert
+            return ((segment & numberChange) << i) | numberSource;
+        }
+    }
+}
